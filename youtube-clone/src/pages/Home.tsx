4af05c7921b7006388a07bd8242b3181195a829d
Rishabh <chaudharyrishabh029@gmail.com>
2024-05-02 11:30:53 +0530

    add on previous commit

--- youtube-clone/src/pages/Home.tsx
+++ youtube-clone/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Header} from "@/components/Header";
 import SideBar from '@/components/SideBar';
+import VideoCard from '@/components/VideoCard';
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Header />
             <SideBar />
+            <VideoCard />
         </div>
     )
 }
