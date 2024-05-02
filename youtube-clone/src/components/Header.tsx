6e356a68ff1d8794e7aabcefd0f2a2eef368a843
Rishabh <chaudharyrishabh029@gmail.com>
2024-05-02 11:47:31 +0530

    added header containers

--- youtube-clone/src/components/Header.tsx
+++ youtube-clone/src/components/Header.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 export const Header = () => {
     return (
-        <div>
-            <div>
-                <h1>hello world</h1>
+        <div className='min-w-full h-20 flex justify-between items-center flex-col'>
+            <div className='min-w-full h-60 bg-black flex justify-around items-center'>
+                <div>
+                    <h1>sidbar</h1>
+                    <h1>youtube logo</h1>
+                </div>
+                <div>
+                    <h1>search bar</h1>
+                    <h1>mic icon</h1>
+                </div>
+                <div>
+                    <h1>create</h1>
+                    <h1>notification</h1>
+                    <h1>profile</h1>
+                </div>
+            </div>
+            <div className='flex h-40 justify-between items-center min-w-80'>
+                <button>buttons</button>
+                <button>buttons</button>
+                <button>buttons</button>
+                <button>buttons</button>
+                <button>buttons</button>
+                <button>buttons</button>
+                <button>buttons</button>
             </div>
         </div>
     )
