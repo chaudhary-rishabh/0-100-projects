46e7875e64174d669ccd401bd9a0a2d2f81671e1
Rishabh <chaudharyrishabh029@gmail.com>
2024-05-02 11:54:07 +0530

    header component changes

--- youtube-clone/src/components/Header.tsx
+++ youtube-clone/src/components/Header.tsx
@@ -4,15 +4,15 @@ export const Header = () => {
     return (
         <div className='min-w-full h-20 flex justify-between items-center flex-col'>
             <div className='min-w-full h-60 bg-black flex justify-around items-center'>
-                <div>
+                <div className='flex'>
                     <h1>sidbar</h1>
                     <h1>youtube logo</h1>
                 </div>
-                <div>
+                <div className='flex'>
                     <h1>search bar</h1>
                     <h1>mic icon</h1>
                 </div>
-                <div>
+                <div className='flex'>
                     <h1>create</h1>
                     <h1>notification</h1>
                     <h1>profile</h1>
--- youtube-clone/src/pages/index.tsx
+++ youtube-clone/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import VideoCard from "@/components/VideoCard";
+import { Header } from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <VideoCard />
     </div>
   );
